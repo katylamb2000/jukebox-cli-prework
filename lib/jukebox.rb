a9c6a4549b41957e3a3d565ab2c35ed6894cfcbf
katy@karstaway.com <katy@karstaway.com>
2018-08-26 17:39:12 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -19,7 +19,7 @@ def help
 end
 
 def list (songs)
- songs.each_with_index{|item, index| do puts "#{index + 1}. #{item}"
+ songs.each_with_index {|item, index| puts "#{index + 1}. #{item}"
   
 
 def play(song_number)
