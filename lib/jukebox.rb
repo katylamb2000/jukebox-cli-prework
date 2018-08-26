c0623622257fb86de68445d87234f7f5a7e69e00
katy@karstaway.com <katy@karstaway.com>
2018-08-26 17:55:07 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -18,22 +18,29 @@ def help
 - exit : exits this program"
 end
 
-def list (songs)
- songs.each_with_index {|item, index| puts "#{index + 1}. #{item}"}
-end
+def list(songs)
+  songs.each_with_index do |song, i|
+    puts "#{i+1}. #{song}"
+  end 
+end 
+
+
 
 def play(songs)
-  puts "Please enter a song name or number"
-  
-  song_name = gets.chomp
+  puts "Please enter a song name or number:"
+  user_request = gets.chomp
+  songs.each_with_index do |song, i|
+    if user_request.to_i == i
+    puts "playing #{songs[i-1]}"
+  elsif user_request == song 
+  puts "playing #{song[i]}"
+else puts "Invalid input, please try again"
+  end 
+end 
+
+end 
 
 
-if gets.chomp == songs.number || songs.name
-  puts "Playing #{gets.chomp}"
-else
-  puts
-"Invalid input, please try again"  
-end
 
 def exit_jukebox
   puts "goodbye"
